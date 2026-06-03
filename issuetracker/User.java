be1282e84c4bb642fe2b5ff47b55ec5a5c1a2021
obibjb <128777885+obibjb@users.noreply.github.com>
2026-06-04 04:38:11 +0900

    Update User.java

--- issuetracker/User.java
+++ issuetracker/User.java
@@ -1,6 +1,8 @@
 package issuetracker;
 
-public class User {
+public class User implements java.io.Serializable {
+    private static final long serialVersionUID = 1L;
+
     public String id;
     public String name;
     public String email;
