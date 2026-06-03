42c747d7b094f43036d9e7d18171cb0bdd0be362
obibjb <128777885+obibjb@users.noreply.github.com>
2026-06-04 04:40:23 +0900

    Update Comment.java

--- issuetracker/Comment.java
+++ issuetracker/Comment.java
@@ -1,6 +1,8 @@
 package issuetracker;
 
-public class Comment {
+public class Comment implements java.io.Serializable {
+    private static final long serialVersionUID = 1L;
+
     public String authorId;
     public String content;
     public String createdAt;
